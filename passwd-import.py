242cd98d4ecf4cedc612cc6a342af008ab683256
Nahum Shalman <nshalman@elys.com>
2009-12-25 14:46:20 -0500

    Brute force, down and dirty import of "passwd" in current directory

---
+++ passwd-import.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import csv
+import subprocess
+
+def ipa_adduser(uid, gid, gecos, homedir, shell, username):
+	firstname, lastname = gecos.split(" ")
+	command = ['ipa-adduser', '--setattr', 'uidnumber=%s' % uid,\
+	'--setattr', 'gidnumber=%s' % gid, '-c', gecos, '-d', homedir, '-s',\
+	shell, "-f", firstname, "-l", lastname, username]
+	print " ".join(command)
+	subprocess.call(command)
+
+passwd_reader = csv.reader( open('passwd'), delimiter=':')
+
+for entry in passwd_reader:
+	username,password,uid,gid,gecos,homedir,shell = entry
+	ipa_adduser(uid, gid, gecos, homedir, shell, username)
